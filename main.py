32b6b96d7ea2e28c1131704ea9212206eb316c96
Maxim Shkividorov <32208852+soutjuseppe@users.noreply.github.com>
2018-10-25 22:26:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import telebot
+
+TOKEN = "640743547:AAHWPuEhr5twym7IkhAQ28ciGRpSRwDG4Vc"
+
+bot = telebot.TeleBot(TOKEN)
+upd = bot.get_updates()
+last_upd = upd[-1]
+
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    print(message.text)
+    if message.text == "ты тупой":
+        bot.send_message(message.chat.id, "Сори, но мираж не играю")
+    elif message.text == "говно из жопы":
+        bot.send_message(message.chat.id, "шоколад")
+    else:
+        bot.send_message(message.chat.id, "Полный крииинж")
+
+
+
+bot.polling(none_stop=True,  interval=0)
